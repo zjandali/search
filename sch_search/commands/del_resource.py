638da2b3d0a62ff7459cc674c1832fbeb0496e7a
Zain Jandali <yzjandali@berkeley.edu>
2024-12-05 22:42:12 -0800

    Update del_resource.py

--- sch_search/commands/del_resource.py
+++ sch_search/commands/del_resource.py
@@ -2,9 +2,6 @@
 {Description}
 Takes a link and passes it through the appropriate parser before deleting it from the database,
 currently weaviate cluster, through weaviate/weaviate_calls.py
-{License_info}
-Scholarhub, Inc. 2021
-
 """
 from sch_search.weaviate.weaviate_calls import delete_link_data
 
