4b4604fae8de2122a23615a19f1417d8ab6b2a1a
Zain Jandali <yzjandali@berkeley.edu>
2024-12-05 22:43:28 -0800

    Update commands_dev.py

--- sch_search/commands/commands_dev.py
+++ sch_search/commands/commands_dev.py
@@ -3,8 +3,6 @@
 Take any dev commands from resource handler and forward them to a database, currently
 to weaviate through weaviate/weaviate_calls.py.
 {License_info}
-Scholarhub, Inc. 2021
-
 """
 from sch_search.weaviate.weaviate_calls_dev import init_weaviate_schema
 
