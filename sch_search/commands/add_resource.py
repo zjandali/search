649c7f1d5d3ef2ce56ce4408f239066035bca9a3
Zain Jandali <yzjandali@berkeley.edu>
2024-12-05 22:45:44 -0800

    Update add_resource.py

--- sch_search/commands/add_resource.py
+++ sch_search/commands/add_resource.py
@@ -1,41 +1,53 @@
-""""
-Takes a link, decides what kind of resource, and passes it through the appropriate
-parser before adding it to the database, currently weaviate cluster,
-through weaviate/weaviate_calls.py
 """
+This module processes links, determines the resource type, and parses the resources using 
+appropriate parsers before adding them to the database. The current implementation uses 
+a Weaviate cluster for storage, with interactions handled through weaviate/weaviate_calls.py.
+"""
+
 from sch_search.parsers.pdf_parser import parse_pdf
 from sch_search.parsers.video_parser import parse_video
 from sch_search.parsers.csv_parser import parse_csv
 
 def add_resources(client, links):
-    ''' Figure out what type each resource is and add it to Weaviate using the appropriate
-        parser.
-        client: Weaviate client
-        links: List of links or paths to resources
-        Returns nothing.
-    '''
+    """
+    Determines the type of each resource from the provided links and adds them to Weaviate 
+    using the corresponding parser.
+
+    Args:
+        client: Weaviate client instance for database operations.
+        links: List of links or paths to resources.
+
+    Returns:
+        None.
+    """
     pdf_links = []
     video_links = []
     csvs = []
 
     for link in links:
+        # Define a filter to check if the link already exists in the database
         where_filter = {
             "path": ["document"],
             "operator": "Equal",
             "valueText": link
         }
-        data = client.query.get("Post", ["document", "_additional {id}"]).\
-            with_where(where_filter).do()
+
+        # Query the database for existing entries
+        data = client.query.get("Post", ["document", "_additional {id}"]) \
+            .with_where(where_filter).do()
+
+        # Classify the resource if not already present in the database
         if not data["data"]["Get"]["Post"]:
-            if link[-4:] == '.pdf':
+            if link.endswith('.pdf'):
                 pdf_links.append(link)
             elif 'youtube.com' in link or 'youtu.be' in link:
                 video_links.append(link)
-            elif link[-4:] == '.csv':
+            elif link.endswith('.csv'):
                 csvs.append(link)
             else:
-                print('not found')
+                print('Resource type not recognized:', link)
 
+    # Parse and add resources to the database
     parse_pdf(client, pdf_links)
     parse_video(client, video_links)
     parse_csv(client, csvs)
